6f3da2984b12b2a3322bb55830292e9c2fe11df6
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2025-09-18 20:38:38 -0300

    Criação das pastas do back e do front bem como a instalação do react e do spring boot para inicio do projeto

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Maderix
 
+# Para inicial o Back-end, utilize o seuinte comando:
+  ./mvnw clean spring-boot:run
+
+# Para alterar a versão do java, basta acessar o pom.xml e alterar o java.version para a versão desejada
---
+++ backend/src/main/java/com/maderix/backend/MaderixApplication.java
@@ -0,0 +1,13 @@
+package com.maderix.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaderixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaderixApplication.class, args);
+	}
+
+}
