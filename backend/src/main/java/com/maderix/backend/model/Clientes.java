2fe83041d06bb46698c1ebdc22d3943a56aa8c84
Gabrie Roberto <152230629+Gabriel-Assis-22@users.noreply.github.com>
2025-10-04 18:11:11 -0300

    Commit

--- backend/src/main/java/com/maderix/backend/model/Clientes.java
+++ backend/src/main/java/com/maderix/backend/model/Clientes.java
@@ -2,14 +2,8 @@ package com.maderix.backend.model;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "CLIENTES")
@@ -20,8 +14,9 @@ public class Clientes {
     @Column(name = "ID_Cliente")
     private Integer ID_Cliente;
 
-    @Column(name = "ID_Empresa")
-    private Integer ID_Empresa;
+    @ManyToOne//CONSTRUÇÃO DO RELACIONAMENTO
+    @JoinColumn(name = "ID_Empresa", nullable = false)
+    private Empresa ID_Empresa;
 
     @Column(name = "NM_Cliente", length = 150, nullable = false)
     private String NM_Cliente;
@@ -32,8 +27,59 @@ public class Clientes {
     @Column(name = "Email", length = 100)
     private String Email;
 
-    @Column(name = "DT_Cad_Cliente", updatable = false)// updatable Desabilita possibilidade de atualização 
+    @Column(name = "DT_Cad_Cliente", updatable = false, nullable = false)// updatable Desabilita possibilidade de atualização
     @CreationTimestamp // Registra a data do Sistema
     private LocalDateTime DT_Cad_Cliente;
 
+
+    //GET E SETS, ALÉM DO CONSTRUTOR PADÃO
+    public Clientes(){}
+
+    public Integer getID_Cliente() {
+        return ID_Cliente;
+    }
+
+    public void setID_Cliente(Integer ID_Cliente) {
+        this.ID_Cliente = ID_Cliente;
+    }
+
+    public Empresa getID_Empresa() {
+        return ID_Empresa;
+    }
+
+    public void setID_Empresa(Empresa ID_Empresa) {
+        this.ID_Empresa = ID_Empresa;
+    }
+
+    public String getNM_Cliente() {
+        return NM_Cliente;
+    }
+
+    public void setNM_Cliente(String NM_Cliente) {
+        this.NM_Cliente = NM_Cliente;
+    }
+
+    public String getTel_Cliente() {
+        return Tel_Cliente;
+    }
+
+    public void setTel_Cliente(String tel_Cliente) {
+        Tel_Cliente = tel_Cliente;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public LocalDateTime getDT_Cad_Cliente() {
+        return DT_Cad_Cliente;
+    }
+
+    public void setDT_Cad_Cliente(LocalDateTime DT_Cad_Cliente) {
+        this.DT_Cad_Cliente = DT_Cad_Cliente;
+    }
 }
--- backend/src/main/java/com/maderix/backend/model/ContasReceber.java
+++ backend/src/main/java/com/maderix/backend/model/ContasReceber.java
@@ -3,14 +3,9 @@ package com.maderix.backend.model;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import com.maderix.backend.repository.EmpresaRepository;
+import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "CONTAS_RECEBER")
@@ -21,13 +16,15 @@ public class ContasReceber {
     @Column(name = "ID_Conta")
     private Integer ID_Conta;
 
-    @Column(name = "ID_Venda")
-    private Integer ID_Venda;
+    @ManyToOne
+    @JoinColumn(name = "ID_Venda", nullable = false)
+    private Vendas ID_Venda;
 
-    @Column(name = "ID_Empresa")
-    private Integer ID_Empresa;
+    @ManyToOne
+    @JoinColumn(name = "ID_Empresa", nullable = false)
+    private Empresa ID_Empresa;
 
-    @Column(name = "Descricao", length = 255, nullable = false)
+    @Column(name = "Descricao", length = 255, nullable = true)
     private String Descricao;
 
     @Column(name = "Valor", nullable = false, precision = 10, scale = 2)
@@ -46,4 +43,78 @@ public class ContasReceber {
     @Column(name = "DT_Cad_Conta", nullable = false)
     @CreationTimestamp
     private LocalDateTime DT_Cad_Conta;
+
+    public ContasReceber(){}
+
+    public Integer getID_Conta() {
+        return ID_Conta;
+    }
+
+    public void setID_Conta(Integer ID_Conta) {
+        this.ID_Conta = ID_Conta;
+    }
+
+    public Vendas getID_Venda() {
+        return ID_Venda;
+    }
+
+    public void setID_Venda(Vendas ID_Venda) {
+        this.ID_Venda = ID_Venda;
+    }
+
+    public Empresa getID_Empresa() {
+        return ID_Empresa;
+    }
+
+    public void setID_Empresa(Empresa ID_Empresa) {
+        this.ID_Empresa = ID_Empresa;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
+
+    public BigDecimal getValor() {
+        return Valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        Valor = valor;
+    }
+
+    public LocalDateTime getData_Vencimento() {
+        return Data_Vencimento;
+    }
+
+    public void setData_Vencimento(LocalDateTime data_Vencimento) {
+        Data_Vencimento = data_Vencimento;
+    }
+
+    public boolean isPago() {
+        return Pago;
+    }
+
+    public void setPago(boolean pago) {
+        Pago = pago;
+    }
+
+    public LocalDateTime getData_Pagamento() {
+        return Data_Pagamento;
+    }
+
+    public void setData_Pagamento(LocalDateTime data_Pagamento) {
+        Data_Pagamento = data_Pagamento;
+    }
+
+    public LocalDateTime getDT_Cad_Conta() {
+        return DT_Cad_Conta;
+    }
+
+    public void setDT_Cad_Conta(LocalDateTime DT_Cad_Conta) {
+        this.DT_Cad_Conta = DT_Cad_Conta;
+    }
 }
