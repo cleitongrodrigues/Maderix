02ee0d4bbafac64d2d141a63275ac5a90560b4c3
Douglas Felipe Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2025-10-17 20:33:00 -0300

    Renomear a Branch

--- backend/src/main/java/com/maderix/backend/controller/ClienteController.java
+++ backend/src/main/java/com/maderix/backend/controller/ClienteController.java
@@ -22,6 +22,7 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 @RequestMapping("api/clientes")
 public class ClienteController {
 
+    
     @Autowired
     private ClientesService clientesService;
 
