d5dd8bde50c101b0910dacc6184829e54f31de83
Douglas Felipe Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2025-10-17 22:44:51 -0300

    Correção dos Controllers

--- backend/src/main/java/com/maderix/backend/controller/EmpresaController.java
+++ backend/src/main/java/com/maderix/backend/controller/EmpresaController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,7 @@ public class EmpresaController {
     @Autowired
     private EmpresaService empresaService;
 
+    @PostMapping
     public ResponseEntity <Empresa> criarEmpresa(@RequestBody Empresa empresa){
         Empresa novaEmpresa = empresaService.salvarEmpresa(empresa);
 
